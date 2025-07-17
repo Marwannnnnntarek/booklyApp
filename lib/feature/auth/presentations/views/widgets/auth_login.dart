ac127c7772448032a4f762d0c1d223b829a1d764
Marwan Tarek <119248877+Marwannnnnntarek@users.noreply.github.com>
2025-07-18 00:27:02 +0300

    finished ogin feature successfuly.

--- lib/feature/auth/presentations/views/widgets/auth_login.dart
+++ lib/feature/auth/presentations/views/widgets/auth_login.dart
@@ -1,8 +1,10 @@
 import 'package:bookly/core/helpers/app_routes.dart';
+import 'package:bookly/feature/auth/presentations/view_model/cubit/auth_cubit.dart';
 import 'package:bookly/feature/auth/presentations/views/widgets/auth_button.dart';
 import 'package:bookly/feature/auth/presentations/views/widgets/auth_text_field.dart';
 import 'package:bookly/feature/auth/presentations/views/widgets/auth_toggle_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class AuthLogin extends StatefulWidget {
@@ -19,43 +21,66 @@ class _AuthLoginState extends State<AuthLogin> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AuthTextField(
-          label: 'Email address',
-          hint: 'Enter your email',
-          controller: emailController,
-        ),
-        const SizedBox(height: 20),
-        AuthTextField(
-          label: 'Password',
-          hint: 'Enter your password',
-          controller: passwordController,
-          isPassword: true,
-          obscureText: _obscurePassword,
-          togglePassword: () {
-            setState(() {
-              _obscurePassword = !_obscurePassword;
-            });
-          },
-        ),
-        const SizedBox(height: 20),
-        AuthButton(
-          label: 'Sign in',
-          onPressed: () {
-            context.go(AppRoutes.home);
-          },
-        ),
-        Align(
-          alignment: Alignment.topLeft,
-          child: AuthToggleText(
-            actionText: 'Forgot password?',
-            onPressed: () {
-              context.push(AppRoutes.reset1);
+    return BlocListener<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is AuthSuccess) {
+          context.go(AppRoutes.home);
+        } else if (state is AuthFailure) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(state.errMessage)));
+        }
+      },
+      child: Column(
+        children: [
+          AuthTextField(
+            label: 'Email address',
+            hint: 'Enter your email',
+            controller: emailController,
+          ),
+          const SizedBox(height: 20),
+          AuthTextField(
+            label: 'Password',
+            hint: 'Enter your password',
+            controller: passwordController,
+            isPassword: true,
+            obscureText: _obscurePassword,
+            togglePassword: () {
+              setState(() {
+                _obscurePassword = !_obscurePassword;
+              });
             },
           ),
-        ),
-      ],
+          const SizedBox(height: 20),
+          BlocBuilder<AuthCubit, AuthState>(
+            builder: (context, state) {
+              final isLoading = state is AuthLoading;
+              return AuthButton(
+                label: 'Sign in',
+                isLoading: isLoading,
+                onPressed:
+                    isLoading
+                        ? null
+                        : () {
+                          context.read<AuthCubit>().login(
+                            emailController.text.trim(),
+                            passwordController.text.trim(),
+                          );
+                        },
+              );
+            },
+          ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: AuthToggleText(
+              actionText: 'Forgot password?',
+              onPressed: () {
+                context.push(AppRoutes.reset1);
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
